be28623649f2ccffbb6612816368f3bfb8317073
vagnergrossklaus <vagnergrossklaus@gmail.com>
2016-03-15 20:02:40 -0300

    ...

--- js/controller/formCtrl.js
+++ js/controller/formCtrl.js
@@ -9,4 +9,41 @@ myApp.controller('FormCtrl', function($scope){
 		email : 'teste@curso.br'
 	}
 
+	$scope.list = [
+		{
+			nomeTarefa : 'Tarefa Teste',
+			dataTarefa : '2016-03-15T18:45:31.000Z'
+		},
+		{
+			nomeTarefa : 'Cortar a grama',
+			dataTarefa : '2016-03-15T18:45:31.000Z'
+		},
+		{
+			nomeTarefa : 'Limpar a casa',
+			dataTarefa : '2016-03-15T18:45:31.000Z'
+		},
+		{
+			nomeTarefa : 'Carpi o lote',
+			dataTarefa : '2016-03-15T18:45:31.000Z'
+		},
+		{
+			nomeTarefa : 'Lavar o carro',
+			dataTarefa : '2016-03-15T18:45:31.000Z'
+		}
+	];
+
+	$scope.cadastrarTarefa = function (data){
+		console.log(data);
+
+		if (data) {
+			$scope.list.push({
+				nomeTarefa : data,
+				dataTarefa : new Date().getTime()
+			});		
+		}
+
+		
+		$scope.task = undefined;
+	}
+
 });
